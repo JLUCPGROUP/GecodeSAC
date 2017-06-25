833baeee634497f7ae54ec09c1b1763860ed1ee6
李哲 <leezear@live.cn>
2017-06-25 22:48:20 +0800

    添加Gecode模型

---
+++ GecodeSAC/GTest/GecodeModel.h
@@ -0,0 +1,34 @@
+/*
+* GecodeModel.h
+*
+*  Created on: 2016Äê9ÔÂ15ÈÕ
+*      Author: leezear
+*/
+#pragma once
+
+#include <gecode/int.hh>
+#include <gecode/minimodel.hh>
+#include <gecode/search.hh>
+
+using namespace Gecode;
+
+class GecodeModel : public Gecode::Space {
+private:
+public:
+	IntVarArray vars_;
+	GecodeModel() {}
+	GecodeModel(bool share, GecodeModel& s)
+		: Space(share, s) {
+		vars_.update(*this, share, s.vars_);
+	}
+
+	virtual Space* copy(bool share) {
+		return new GecodeModel(share, *this);
+	}
+
+	void print(void) const {
+		std::cout << vars_ << std::endl;
+	}
+
+	virtual ~GecodeModel() {}
+};
--- GecodeSAC/GTest/main.cpp
+++ GecodeSAC/GTest/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include "XMLBuilder.h"
-
+#include "BuildGecodeModel.h"
 using namespace std;
 using namespace cp;
 using namespace cp::model;
@@ -14,6 +14,32 @@ int main()
 	XMLBuilder path_builder(X_PATH, XRT_BM_PATH);
 	string bm_path = path_builder.GetBMFile();
 	cout << bm_path << endl;
+	XMLBuilder builder(bm_path, XRT_BM);
+	XMLModel* xml_model = new XMLModel();
+	builder.GenerateModelFromXml(xml_model);
+	GecodeModel* gm = new GecodeModel;
+	BuildGecodeModel(xml_model, gm);
+	DestroyEXTModel(xml_model);
 
+	//IntAFC afc(*gm, gm->vars_);
+	branch(*gm, gm->vars_, INT_VAR_AFC_SIZE_MAX(0), INT_VAL_MIN());
+	//branch(*gm, gm->vars_, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
+	//branch(*gm, gm->vars_, INT_VAR_DEGREE_SIZE_MIN(), INT_VAL_MIN());
+
+	DFS<GecodeModel> ee(gm);
+	delete gm;
+	int i = 0;
+	//while (GecodeModel* ss = ee.next()) {
+	//	//ss->print();
+	//	++i;
+	//	delete ss;
+	//}
+	if (GecodeModel* ss = ee.next()) {
+		ss->print();
+		cout << "nodes = " << ee.statistics().node << endl;
+		delete ss;
+	}
+	//cout << "|solutions| = " << i << endl;
+	cout << "---end---" << endl;
 	return 0;
 }
