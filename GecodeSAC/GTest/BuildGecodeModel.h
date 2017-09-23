da61a94c45c2c8cdf686c4b8f4d619693f04e223
李哲 <leezear@live.cn>
2017-09-23 22:27:43 +0800

    sdasda

--- GecodeSAC/GTest/BuildGecodeModel.h
+++ GecodeSAC/GTest/BuildGecodeModel.h
@@ -8,143 +8,148 @@
 #ifndef BUILDGECODEMODEL_H_
 #define BUILDGECODEMODEL_H_
 
-#include "XMLModel.h"
+#include "model/HModel.h"
 #include "GecodeModel.h"
-using namespace cp::model;
+using namespace cudacp;
 using namespace Gecode;
+//
+////************************************
+//// Method:    IsEqual
+//// FullName:  IsEqual
+//// Access:    public 
+//// Returns:   bool
+//// Qualifier: 判断元组是否相等
+//// Parameter: const int * lh
+//// Parameter: const int * rh
+//// Parameter: const int & len	元组长度
+////************************************
+//inline bool IsEqual(const int* lh, const int* rh, const int& len) {
+//	for (int i = 0; i < len; ++i)
+//		if (lh[i] != rh[i])
+//			return false;
+//
+//	return true;
+//}
+//
+////************************************
+//// Method:    GetIntTuple
+//// FullName:  GetIntTuple
+//// Access:    public 
+//// Returns:   void
+//// Qualifier: 从序号获得约束元组
+//// Parameter: int idx	序号
+//// Parameter: int * t	元组	
+//// Parameter: const XMLDomain * * ds	scope(c)
+////************************************
+//inline void GetIntTuple(int idx, int *t, const int& len, XDom **ds) {
+//	for (int i = (len - 1); i >= 0; --i) {
+//		t[i] = ds[i]->values[(idx % (ds[i]->size))];
+//		idx /= (ds[i]->size);
+//	}
+//}
 
-//************************************
-// Method:    IsEqual
-// FullName:  IsEqual
-// Access:    public 
-// Returns:   bool
-// Qualifier: 判断元组是否相等
-// Parameter: const int * lh
-// Parameter: const int * rh
-// Parameter: const int & len	元组长度
-//************************************
-inline bool IsEqual(const int* lh, const int* rh, const int& len)
-{
-	for (int i = 0; i < len; ++i)
-		if (lh[i] != rh[i])
-			return false;
+void BuildGecodeModel(const  HModel* h_model, GecodeModel* g_model) {
+	//if (h_model->property.vs_size == 1) {
+	//	*d = &xmodel->doms[0];
+	//	//		g_model->vars_ = IntVarArray(*g_model, xmodel->vs_size, d.values[0], d.values[d.size - 1]);
+	//	g_model->vars_ = IntVarArray(*g_model, xmodel->vs_size, d->values[0], d->values[d->size - 1]);
+	//}
+	//else if (xmodel->ds_size != 1 && xmodel->vs_size != 1) {
+	//	g_model->vars_ = IntVarArray(*g_model, xmodel->vs_size);
 
-	return true;
-}
+	//	for (int i = 0; i < xmodel->vs_size; ++i) {
+	//		XMLVariable* v = &xmodel->variables[i];
+	//		g_model->vars_[i] = IntVar(*g_model, IntSet(xmodel->domains[v->dm_id].values, xmodel->domains[v->dm_id].size));
+	//		cout << i << ":" << g_model->vars_[i].size() << endl;
+	//	}
+	//}
 
-//************************************
-// Method:    GetIntTuple
-// FullName:  GetIntTuple
-// Access:    public 
-// Returns:   void
-// Qualifier: 从序号获得约束元组
-// Parameter: int idx	序号
-// Parameter: int * t	元组	
-// Parameter: const XMLDomain * * ds	scope(c)
-//************************************
-inline void GetIntTuple(int idx, int *t, const int& len, XMLDomain **ds)
-{
-	for (int i = (len - 1); i >= 0; --i)
-	{
-		t[i] = ds[i]->values[(idx % (ds[i]->size))];
-		idx /= (ds[i]->size);
+	g_model->vars_ = IntVarArray(*g_model, h_model->property.vs_size);
+
+	for (size_t i = 0; i < h_model->property.vs_size; ++i) {
+		HVar* v = h_model->vars[i];
+		g_model->vars_[i] = IntVar(*g_model, IntSet(h_model->vars[i]->vals, h_model->vars[i]->size));
 	}
-}
 
-void BuildGecodeModel(const XMLModel *xmodel, GecodeModel* g_model)
-{
-	if (xmodel->ds_size == 1)
-	{
-		XMLDomain *d = &xmodel->domains[0];
-		//		g_model->vars_ = IntVarArray(*g_model, xmodel->vs_size, d.values[0], d.values[d.size - 1]);
-		g_model->vars_ = IntVarArray(*g_model, xmodel->vs_size, d->values[0], d->values[d->size - 1]);
-	}
-	else if (xmodel->ds_size != 1 && xmodel->vs_size != 1)
-	{
-		g_model->vars_ = IntVarArray(*g_model, xmodel->vs_size);
+	for (size_t i = 0; i < h_model->property.ts_size; ++i) {
+		HTab* tab = h_model->tabs[i];
+		TupleSet ts;
 
-		for (int i = 0; i < xmodel->vs_size; ++i)
-		{
-			XMLVariable* v = &xmodel->variables[i];
-			g_model->vars_[i] = IntVar(*g_model, IntSet(xmodel->domains[v->dm_id].values, xmodel->domains[v->dm_id].size));
-			cout << i << ":" << g_model->vars_[i].size() << endl;
-		}
+		for (size_t j = 0; j < tab->size; ++j)
+			ts.add(IntArgs(tab->arity, tab->tuples[j]));
+		ts.finalize();
+
+		IntVarArgs scope;
+		for (int j = 0; j < tab->arity; ++j) 
+			scope << g_model->vars_[tab->scope[j]];
+
+		extensional(*g_model, scope, ts);
 	}
 
-	if (xmodel->features.rel_type == REL_TABULAR)
-	{
-		int* tpl = new int[xmodel->features.arity];
-		XMLDomain** ds = new XMLDomain*[xmodel->features.arity];
-		int num_tuples;
+	//if (xmodel->features.rel_type == REL_TABULAR) {
+	//	int* tpl = new int[xmodel->features.arity];
+	//	XMLDomain** ds = new XMLDomain*[xmodel->features.arity];
+	//	int num_tuples;
 
-		for (int i = 0; i < xmodel->cs_size; ++i)
-		{
-			int k = 0;
-			XMLConstraint *c = &xmodel->constraints[i];
-			TupleSet ts;
-			XMLRelation* r = &xmodel->relations[c->re_id];
-			XMLVariable* v;
-			XMLDomain* d;
+	//	for (int i = 0; i < xmodel->cs_size; ++i) {
+	//		int k = 0;
+	//		XMLConstraint *c = &xmodel->constraints[i];
+	//		TupleSet ts;
+	//		XMLRelation* r = &xmodel->relations[c->re_id];
+	//		XMLVariable* v;
+	//		XMLDomain* d;
 
-			if (r->semantices == SEM_SUPPORT)
-			{
-				for (int j = 0; j < r->size; ++j)
-					ts.add(IntArgs(c->arity, r->tuples[j]));
+	//		if (r->semantices == SEM_SUPPORT) {
+	//			for (int j = 0; j < r->size; ++j)
+	//				ts.add(IntArgs(c->arity, r->tuples[j]));
 
-				ts.finalize();
-			}
-			else
-			{
-				num_tuples = 1;
+	//			ts.finalize();
+	//		}
+	//		else {
+	//			num_tuples = 1;
 
-				for (int j = 0; j < r->arity; ++j)
-				{
-					v = &xmodel->variables[j];
-					d = &xmodel->domains[v->dm_id];
-					ds[j] = d;
-					num_tuples *= d->size;
-				}
+	//			for (int j = 0; j < r->arity; ++j) {
+	//				v = &xmodel->variables[j];
+	//				d = &xmodel->domains[v->dm_id];
+	//				ds[j] = d;
+	//				num_tuples *= d->size;
+	//			}
 
-				for (int j = 0; j < num_tuples; ++j)
-				{
-					GetIntTuple(j, tpl, r->arity, ds);
+	//			for (int j = 0; j < num_tuples; ++j) {
+	//				GetIntTuple(j, tpl, r->arity, ds);
 
-					//支持
-					if (k < r->size)
-					{
-						if (!IsEqual(r->tuples[k], tpl, r->arity))
-						{
-							ts.add(IntArgs(c->arity, tpl));
-						}
-						else
-							++k;
-					}
-					else
-						ts.add(IntArgs(c->arity, tpl));
-				}
+	//				//支持
+	//				if (k < r->size) {
+	//					if (!IsEqual(r->tuples[k], tpl, r->arity)) {
+	//						ts.add(IntArgs(c->arity, tpl));
+	//					}
+	//					else
+	//						++k;
+	//				}
+	//				else
+	//					ts.add(IntArgs(c->arity, tpl));
+	//			}
 
-				ts.finalize();
-			}
+	//			ts.finalize();
+	//		}
 
-			IntVarArgs scope;
+	//		IntVarArgs scope;
 
-			for (int j = 0; j < c->arity; ++j)
-			{
-				scope << g_model->vars_[c->scope[j]];
-			}
-			//IntVarArray s;
-			extensional(*g_model, scope, ts);
-		}
+	//		for (int j = 0; j < c->arity; ++j) {
+	//			scope << g_model->vars_[c->scope[j]];
+	//		}
+	//		//IntVarArray s;
+	//		extensional(*g_model, scope, ts);
+	//	}
 
-		delete[] tpl;
-		delete[] ds;
+	//	delete[] tpl;
+	//	delete[] ds;
 
-		tpl = NULL;
-		ds = NULL;
-	}
-	else
-	{
-	}
+	//	tpl = NULL;
+	//	ds = NULL;
+	//}
+	//else {
+	//}
 }
 
 #endif /* BUILDGECODEMODEL_H_ */
--- GecodeSAC/GTest/main.cpp
+++ GecodeSAC/GTest/main.cpp
@@ -1,64 +1,64 @@
 #include <iostream>
 #include <string>
-#include "XMLBuilder.h"
-#include "BuildGecodeModel.h"
 #include <time.h>
+#include "BuildGecodeModel.h"
+#include "model/XBuilder.h"
+#include "model/HBuilder.h"
 #include "SAC1.h"
 using namespace std;
-using namespace cp;
-using namespace cp::model;
-using namespace cp::parse;
-using namespace cp::algorithm;
+using namespace cudacp;
 
 const string X_PATH = "BMPath.xml";
 
-int main()
-{
+int main() {
 	clock_t  begin, end;
-	XMLBuilder path_builder(X_PATH, XRT_BM_PATH);
+	XBuilder path_builder(X_PATH, XRT_BM_PATH);
 	string bm_path = path_builder.GetBMFile();
 	cout << bm_path << endl;
-	XMLBuilder builder(bm_path, XRT_BM);
-	XMLModel* xml_model = new XMLModel();
-	builder.GenerateModelFromXml(xml_model);
+	XBuilder builder(bm_path, XRT_BM);
+	XModel* xmodel = new XModel();
+	builder.GenerateModelFromXml(xmodel);
+	HModel* hmodel = new HModel();
+	HBuilder hbuilder(xmodel, hmodel);
 	GecodeModel* gm = new GecodeModel;
-	BuildGecodeModel(xml_model, gm);
-	DestroyEXTModel(xml_model);
+	BuildGecodeModel(hmodel, gm);
 
 	//IntAFC afc(*gm, gm->vars_);
-	//branch(*gm, gm->vars_, INT_VAR_AFC_SIZE_MAX(0), INT_VAL_MIN());
-	branch(*gm, gm->vars_, INT_VAR_SIZE_MIN(), INT_VALUES_MIN());
+	branch(*gm, gm->vars_, INT_VAR_AFC_SIZE_MAX(0), INT_VAL_MIN());
+	//branch(*gm, gm->vars_, INT_VAR_SIZE_MIN(), INT_VALUES_MIN());
 	//branch(*gm, gm->vars_, INT_VAR_DEGREE_SIZE_MIN(), INT_VAL_MIN());
 
-	//DFS<GecodeModel> ee(gm);
-	//delete gm;
+	DFS<GecodeModel> ee(gm);
+	delete gm;
 	//int i = 0;
-	//begin = clock();
+	begin = clock();
 	//while (GecodeModel* ss = ee.next()) {
 	//	//ss->print();
 	//	++i;
 	//	delete ss;
 	//}
-	//end = clock();
-	//if (GecodeModel* ss = ee.next()) {
-	//	ss->print();
-	//	cout << "nodes = " << ee.statistics().node << endl;
-	//	delete ss;
-	//}
+	if (GecodeModel* ss = ee.next()) {
+		ss->print();
+		cout << "nodes = " << ee.statistics().node << endl;
+		delete ss;
+	}
+	end = clock();
 	//cout << "|solutions| = " << i << endl;
 
 	//--------------------------------------------------------
-	SAC_1 sac1(gm);
-	begin = clock();
+	//SAC_1 sac1(gm);
+	//begin = clock();
 	//gm->status();
 	//const Choice* ch = gm->choice();
 	//unsigned int n = ch->alternatives();
 	//ch->
 	//std::cout << "n = " << n << std::endl;
-	sac1.enforce();
-	end = clock();
-	delete gm;
+	//sac1.enforce();
+	//end = clock();
+	//delete gm;
 
+	delete hmodel;
+	delete xmodel;
 	cout << "execute time = " << end - begin << endl;
 	cout << "---end---" << endl;
 	return 0;
